a02dc6eee41f9cad41aa744c4970693b917ffeca
saintabomination <saint.abomination@protonmail.com>
2022-04-22 15:29:17 +0200

    Initialising game and drawing it

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,49 @@
+#include <stdlib.h>
 #include <iostream>
+
+void clearScreen()
+{
+  system("clear");
+}
+
+void initGame(int (&game)[10][10])
+{
+  for (int y = 0; y < 10; y++)
+  {
+    for (int x = 0; x < 10; x++)
+    {
+      game[y][x] = 1;
+    }
+  }
+}
+
+void drawGame(int game[10][10])
+{
+  for (int y = 0; y < 10; y++)
+  {
+    for (int x = 0; x < 10; x++)
+    {
+      switch (game[y][x])
+      {
+        case 1:
+          std::cout << '#';
+
+        default:
+          std::cout << ' ';
+      }
+      std::cout << ' ';
+    }
+    std::cout << '\n';
+  }
+}
 
 int main()
 {
-  std::cout << "Initial Commit!" << std::endl;
+  clearScreen();
+
+  int game[10][10];
+  initGame(game);
+  drawGame(game);
+
   return 0;
 }
